6f508317d83fdde67d7a5a1e46c88809c4fd6959
Samuel Barragan <samuel.barragan3417@gmail.com>
2024-07-23 21:22:27 -0700

    Formateo de codigo No Niños

--- src/app/_components/no-ninos/NoNinos.tsx
+++ src/app/_components/no-ninos/NoNinos.tsx
@@ -1,17 +1,22 @@
 import styles from "./no-ninos.module.scss"
 
 export const NoNinos = () => {
-	return <>
-		<div className={styles.container}> 
-			<div className={styles.container__box}>
-				<span className={styles.title}>NO nIñoS</span>
+	return (
+		<>
+			<div className={styles.container}>
+				<div className={styles.container__box}>
+					<span className={styles.title}>NO nIñoS</span>
+				</div>
+				<div className={styles.line}></div>
+				<div className={styles.text}>
+					<p className={styles.text__title}>ADULTOS SOLAMENTE SIN EXCEPCIÓN</p>
+					<p className={styles.text__paragraph}>
+						Esperamos puedan encontrar un lugar seguro para dejar a sus hijos al
+						cuidado <br /> de otros y comprender nuestra decisión de hacer de
+						este evento uno <b>SIN NIÑOS</b>.
+					</p>
+				</div>
 			</div>
-			<div className={styles.line}></div>
-			<div className={styles.text}>
-				<p className={styles.text__title}>ADULTOS SOLAMENTE SIN EXCEPCIÓN</p>
-				<p className={styles.text__paragraph}>Esperamos puedan encontrar un lugar seguro para dejar a sus hijos al cuidado <br /> de otros y comprender nuestra decisión de hacer de este evento uno <b>SIN NIÑOS</b>.</p>
-			</div>
-		</div>
-	
-	</>
+		</>
+	)
 }
