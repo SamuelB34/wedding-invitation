32057aa772f50d308ff6fa2ce57219c1678f0dcd
marlene-araiza <marlene_araiza@outlook.com>
2024-07-24 01:28:46 -0700

    Componente Vestimenta terminado

--- src/app/_components/vestimenta/Vestimenta.tsx
+++ src/app/_components/vestimenta/Vestimenta.tsx
@@ -11,7 +11,7 @@ export const Vestimenta = () => {
                     <span className={styles.text}>FORMAL COLOR NEGRO</span>
                 </div>
                 <div className={styles.diamond}></div>
-                <div className={styles.paragraph}><p>¡Queremos lograr fotos cohesivas ese dia! <br />
+                <div className={styles.paragraph}><p>¡Queremos lograr fotos cohesivas ese día! <br />
                 Les pedimos <b>por favor respeten nuestra elección</b>.</p></div>
 			</div>
 		</>
