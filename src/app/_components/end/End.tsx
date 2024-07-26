76f2262fda56967bcf71ea57fcf97529568fc79b
marlene-araiza <marlene_araiza@outlook.com>
2024-07-25 19:32:51 -0700

    Componente End terminado

--- src/app/_components/end/End.tsx
+++ src/app/_components/end/End.tsx
@@ -1,15 +1,16 @@
-import { Counter } from "../counter/Counter"
 import styles from "./end.module.scss"
+import { Counter } from "../counter/Counter"
 
 export const End = () => {
 	return (
 		<>
 			<div className={styles.container}>
-				<div className={styles.title__image}></div>
-                <div className={styles.counter}><Counter/></div>
+				<div className={styles.title__image}><img src="./end/Vector.png" alt="" /></div>
+                <div></div>
+                <div><Counter/></div>
                 <div className={styles.text}>
                     <p className={styles.text__title}>¡NOS VEMOS PRONTO!</p>
-                    <p className={styles.text__paragraph}>Si aún no confirmas tu asistencia, <b href="#">¡haz click aquí!</b></p>
+                    <p className={styles.text__paragraph}>Si aún no confirmas tu asistencia, <a href="#">¡haz click aquí!</a></p>
                 </div>
                 <div className={styles.line}></div>
                 <div className={styles.nav}>
